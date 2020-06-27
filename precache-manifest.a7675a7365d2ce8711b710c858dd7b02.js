d2492f48cfc7d7f3779521ffac384b5875c66e9e
Firee80 <firee80@gmail.com>
2020-06-28 00:50:15 +0300

    Updates

---
+++ precache-manifest.a7675a7365d2ce8711b710c858dd7b02.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697b407deac2dd36f52403dd9d468a54",
+    "url": "/chess-project/index.html"
+  },
+  {
+    "revision": "b42311c4a505c2d1f77b",
+    "url": "/chess-project/static/js/main.js"
+  }
+]);
